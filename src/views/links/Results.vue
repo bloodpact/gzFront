e2016391236c70bb6ea3ddf068afa0bfc98becaa
bloodpact <toxep@mail.ru>
2019-03-04 22:35:27 +0300

    added duplicate filter

--- src/views/links/Results.vue
+++ src/views/links/Results.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="card-elements item-list">
-        <div class="card m-2 custom" v-for="(link, index) in links"
+        <div class="card m-2 custom result" v-for="(link, index) in links"
              :key="link._id"
              :index="index"
              style="width: 18rem;">
-            <div class="card-body" v-if="links !== null">
+            <div class="card-body">
                 <h5  class="card-title">{{link.title._text}}</h5>
                 <p v-html="link.description._text" class="card-text"></p>
-                <p class="card-text">{{link.link._text}}</p>
+                <a class="card-text" :href="'http://zakupki.gov.ru' +link.link._text">{{link.title._text}}</a>
                 <router-link :to="'/links/edit/' + link._id"  exact>
                     <button class="btn btn-secondary"  >Edit </button>
                 </router-link>
@@ -18,6 +18,8 @@
 </template>
 <script>
     import * as links from '../../services/LinksService'
+    import * as viewHelper from '../../services/ViewService'
+    import _ from 'lodash'
     import moment from 'moment'
     export default{
         data:function () {
@@ -29,13 +31,21 @@
         },
         name: 'result',
         mounted (){
-            this.from =  (moment(new Date()).subtract(1, 'days')).format("DD.MM.YYYY");
-            this.to =  (moment(new Date())).format("DD.MM.YYYY");
-            links.findTenders(this.from, this.to)
-                    .then(res =>{
-                //in response from gz is empty(null) filter it
-                this.links = res.data.filter(el => el !==null)
-            })
+          this.filteredLinks
+        },
+        computed:{
+            filteredLinks(){
+                this.from = (moment(new Date()).subtract(1, 'days')).format("DD.MM.YYYY");
+                this.to =  (moment(new Date())).format("DD.MM.YYYY");
+                links.findTenders(this.from, this.to)
+                     .then(res =>{
+                    //in response from gz is empty(null) filter it
+                    const links = res.data.filter(el => el !==null)
+                    //remove duplicates
+                    const filtered = _.uniqBy(links, 'title._text')
+                    this.links = filtered
+                })
+            }
         }
     }
 </script>
